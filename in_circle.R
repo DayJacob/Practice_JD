58b0fc86e93a14954a46264f4c72f65df455718f
Jacob Day <jacoblovesmath@gmail.com>
2019-08-17 14:21:12 -0400

    Update in_circle.R
    Updated default lwd

--- in_circle.R
+++ in_circle.R
@@ -8,8 +8,8 @@ approx_pi <- function(n) {
   dy <- runif(n, min=0, max=2)
   ix <- which(1 >= (dx-1)**2 + (dy-1)**2)
   
-  points(dx[ix], dy[ix], col='red', pch=20, lwd=3)
-  points(dx[-ix], dy[-ix], col='blue', pch=20, lwd=3)
+  points(dx[ix], dy[ix], col='red', pch=20, lwd=2)
+  points(dx[-ix], dy[-ix], col='blue', pch=20, lwd=2)
   
   area <- 4 * length(ix)/n
   print(area)
